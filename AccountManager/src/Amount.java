0916260cab026c368ad5e66566db9cb76a356ed1
Feruz Tursunov <feruztursunovv@gmail.com>
2024-09-25 23:24:26 +0600

    Adding Main programm

---
+++ AccountManager/src/Amount.java
@@ -0,0 +1,18 @@
+public class Amount{
+    private double amount;
+    private TransactionType transactionType;
+
+    public Amount(double amount, TransactionType transactionType){
+        this.amount = amount;
+        this.transactionType = transactionType;
+    }
+
+    public double getAmount(){
+        return amount;
+    }
+
+    public TransactionType getTransactionType() {
+        return transactionType;
+    }
+    
+}
---
+++ AccountManager/src/PersonalAccount.java
@@ -0,0 +1,62 @@
+import java.util.ArrayList;
+
+public class PersonalAccount {
+    private int accountNumber;
+    private String accountHolder;
+    private double balance;
+    private ArrayList<Amount> transactions;  // We use ArrayList for dynamic storage.
+
+    public PersonalAccount(int accountNumber, String accountHolder) {
+        this.accountNumber = accountNumber;
+        this.accountHolder = accountHolder;
+        this.balance = 0.0;
+        this.transactions = new ArrayList<>();  // Initialize the transaction list
+    }
+
+    public void deposit(double amount) {
+        if (amount > 0) {
+            Amount transaction = new Amount(amount, TransactionType.DEPOSIT);
+            transactions.add(transaction);
+            balance += amount;
+            System.out.println("Deposited: $" + amount);
+        } else {
+            System.out.println("Deposit amount must be positive.");
+        }
+    }
+
+    public void withdraw(double amount) {
+        if (amount > 0 && amount <= balance) {
+            Amount transaction = new Amount(amount, TransactionType.WITHDRAWAL);
+            transactions.add(transaction);
+            balance -= amount;
+            System.out.println("Withdrew: $" + amount);
+        } else if (amount > balance) {
+            System.out.println("Insufficient balance for withdrawal.");
+        } else {
+            System.out.println("Withdrawal amount must be positive.");
+        }
+    }
+
+    public void printTransactionHistory() {
+        if (transactions.isEmpty()) {
+            System.out.println("No transactions available.");
+        } else {
+            System.out.println("Transaction History:");
+            for (Amount transaction : transactions) {
+                System.out.println(transaction.getTransactionType() + ": $" + transaction.getAmount());
+            }
+        }
+    }
+
+    public double getBalance() {
+        return balance;
+    }
+
+    public int getAccountNumber() {
+        return accountNumber;
+    }
+
+    public String getAccountHolder() {
+        return accountHolder;
+    }
+}
